7771b8f0848f493a714518b905d618cdd2e9c5b5
plokm5151 <plokm85222131+1@gmail.com>
2023-03-25 21:42:26 +0800

    add module.py file

---
+++ module.py
@@ -0,0 +1,12 @@
+import list
+
+
+class module:
+    def __init__(self):
+        list.score().my_print()
+        list.score().print_native_number()
+        list.score().print_range()
+        list.score().sort_and_print()
+
+
+Test=module()
